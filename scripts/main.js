431b4a64bd7520cd0ef34591f6b02661aab22677
TB <snorka@live.de>
2020-01-22 22:27:27 +0100

    got flexbox to work

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
-let button = document.querySelector('button');
-let image = document.querySelector('img');
+let m_image = document.querySelector('img');
+let images = document.getElementsByClassName('album');
 
-image.onclick = function(){
-    let src = image.getAttribute('src');
-    if(src === 'images/277172289113072019012526.png'){
-        image.setAttribute('src', 'images/277179398123052019060105.png');
-    } else{
-        image.setAttribute('src', 'images/277172289113072019012526.png');
+console.log(images);
+
+for (let img of images){
+    img.onclick = function(){
+        let src = img.getAttribute('src');
+        m_image.setAttribute('src', src);
+        //console.log("lol", src);
     }
-
 }
 
 /*
-Kommentare sind toll.
-*/
+m_image.onclick = function(){
+    let src = m_image.getAttribute('src');
+    if(src === 'images/277172289113072019012526.png'){
+        m_image.setAttribute('src', 'images/277179398123052019060105.png');
+    } else{
+        m_image.setAttribute('src', 'images/277172289113072019012526.png');
+    }
+}*/
