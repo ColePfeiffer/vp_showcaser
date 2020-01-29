7da3c9e8a60c5652ade1e98c90312556e31c23f5
Unknown <klav@live.de>
2020-01-29 15:09:03 +0100

    imgur einbindung incoming, slider gemacht

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,36 @@
-let m_image = document.querySelector('img');
-let images = document.getElementsByClassName('album');
+let m_image = document.querySelector("img");
+let images = document.getElementsByClassName("album");
+let button = document.getElementById("button-album");
 
 console.log(images);
 
 for (let img of images){
     img.onclick = function(){
-        let src = img.getAttribute('src');
-        m_image.setAttribute('src', src);
+        let src = img.getAttribute("src");
+        m_image.setAttribute("src", src);
         //console.log("lol", src);
     }
 }
+
+let slider = document.getElementById("slider-height");
+let sliderOutput = document.getElementById("slider-height-output");
+
+sliderOutput.innerHTML = slider.value+" px";
+
+slider.oninput = function(){
+    sliderOutput.innerHTML = this.value+" px";
+    console.log(this.value+"px");
+    for (img of images){
+        img.style.height = this.value+"px";
+    }
+}
+
+button.onclick = function(){
+    console.log("yolo");
+
+
+};
+
 
 /*
 m_image.onclick = function(){
